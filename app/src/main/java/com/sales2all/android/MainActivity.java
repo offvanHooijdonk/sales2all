90a8240fca5a58cbbd111c9bd572c1f93485eaf2
offvanhooijdonk <offspring25@gmail.com>
2016-04-08 18:19:59 +0300

    Assert for FAB added

--- app/src/main/java/com/sales2all/android/MainActivity.java
+++ app/src/main/java/com/sales2all/android/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        assert fab != null;
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
